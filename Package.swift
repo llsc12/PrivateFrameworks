9f9be55181114fcbd047fac8af5913907bb0341f
Lakhan Lothiyi <llsc12.dev@gmail.com>
2024-10-20 03:48:42 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,61 +4,82 @@
 import PackageDescription
 import Foundation
 
-let sourcesDirectory = URL(fileURLWithPath: #file)
-    .deletingLastPathComponent()
-    .appendingPathComponent("Sources")
+//let sourcesDirectory = URL(fileURLWithPath: #file)
+//    .deletingLastPathComponent()
+//    .appendingPathComponent("Sources")
+//
+//func tbd(for name: String) -> String {
+//  sourcesDirectory
+//    .appending(path: "CFrameworks")
+//    .appending(path: "tbds")
+//    .appending(component: "\(name).tbd")
+//    .path()
+//}
+//
+//let products: [PackageDescription.Product] = {
+//  let dir = try! FileManager.default.contentsOfDirectory(at: sourcesDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+//  let folders = dir.map(\.lastPathComponent)
+//  let products = folders.filter { $0 != "CFrameworks" && $0 != "ObjCShims" }
+//  return products.map { .library(name: $0, targets: [$0]) }
+//}()
+//
+//let targets: [PackageDescription.Target] = {
+//  var targets = [PackageDescription.Target]()
+//  let dir = try! FileManager.default.contentsOfDirectory(at: sourcesDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+//  let folders = dir.map(\.lastPathComponent)
+//  let foundTargets = folders.filter { $0 != "CFrameworks" && $0 != "ObjCShims" }
+//  
+//  // add cframeworks and test first
+//  targets.append(contentsOf: [
+//    .systemLibrary(name: "CFrameworks", path: nil, pkgConfig: nil, providers: nil),
+//    .target(name: "ObjCShims", dependencies: ["CFrameworks"], publicHeadersPath: "include"),
+//    .testTarget(
+//      name: "PrivateFrameworksTests",
+//      dependencies: [.byName(name: "CFrameworks"), .byName(name: "ObjCShims")] + foundTargets.map { .byName(name: $0) }
+//    )
+//  ])
+//  
+//  // now add known targets, and link tbd if exists
+//  targets.append(
+//    contentsOf:
+//      foundTargets.map({ target in
+//        if FileManager.default.fileExists(atPath: tbd(for: target)) {
+//          return .target(
+//            name: target, dependencies: ["CFrameworks", "ObjCShims"],
+//            linkerSettings: [.unsafeFlags([tbd(for: target)])]
+//          )
+//        } else {
+//          return .target(name: target, dependencies: ["CFrameworks", "ObjCShims"])
+//        }
+//      })
+//  )
+//  
+//  return targets
+//}()
+//
+//let package = Package(
+//  name: "PrivateFrameworks",
+//  products: products,
+//  targets: targets
+//)
 
-func tbd(for name: String) -> String {
-  sourcesDirectory
-    .appending(path: "CFrameworks")
-    .appending(path: "tbds")
-    .appending(component: "\(name).tbd")
-    .path()
-}
-
-let products: [PackageDescription.Product] = {
-  let dir = try! FileManager.default.contentsOfDirectory(at: sourcesDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-  let folders = dir.map(\.lastPathComponent)
-  let products = folders.filter { $0 != "CFrameworks" && $0 != "ObjCShims" }
-  return products.map { .library(name: $0, targets: [$0]) }
-}()
-
-let targets: [PackageDescription.Target] = {
-  var targets = [PackageDescription.Target]()
-  let dir = try! FileManager.default.contentsOfDirectory(at: sourcesDirectory, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-  let folders = dir.map(\.lastPathComponent)
-  let foundTargets = folders.filter { $0 != "CFrameworks" && $0 != "ObjCShims" }
-  
-  // add cframeworks and test first
-  targets.append(contentsOf: [
+let package = Package(
+  name: "PrivateFrameworks",
+  products: [
+    .library(name: "Calculate", targets: ["Calculate"]),
+    .library(name: "Dictionary", targets: ["Dictionary"]),
+  ],
+  targets: [
     .systemLibrary(name: "CFrameworks", path: nil, pkgConfig: nil, providers: nil),
     .target(name: "ObjCShims", dependencies: ["CFrameworks"], publicHeadersPath: "include"),
     .testTarget(
       name: "PrivateFrameworksTests",
-      dependencies: [.byName(name: "CFrameworks"), .byName(name: "ObjCShims")] + foundTargets.map { .byName(name: $0) }
-    )
-  ])
-  
-  // now add known targets, and link tbd if exists
-  targets.append(
-    contentsOf:
-      foundTargets.map({ target in
-        if FileManager.default.fileExists(atPath: tbd(for: target)) {
-          return .target(
-            name: target, dependencies: ["CFrameworks", "ObjCShims"],
-            linkerSettings: [.unsafeFlags([tbd(for: target)])]
-          )
-        } else {
-          return .target(name: target, dependencies: ["CFrameworks", "ObjCShims"])
-        }
-      })
-  )
-  
-  return targets
-}()
-
-let package = Package(
-  name: "PrivateFrameworks",
-  products: products,
-  targets: targets
+      dependencies: [.byName(name: "CFrameworks"), .byName(name: "ObjCShims")] + [
+        "Calculate",
+        "Dictionary"
+      ]
+    ),
+    .target(name: "Calculate", dependencies: [.byName(name: "CFrameworks"), .byName(name: "ObjCShims")]),
+    .target(name: "Dictionary", dependencies: [.byName(name: "CFrameworks"), .byName(name: "ObjCShims")])
+  ]
 )
